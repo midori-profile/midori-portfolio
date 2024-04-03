db2932631399b8d8aa8224c93977b85a8b1da5df
ivy-tian <ceada.apple@gmail.com>
2024-04-03 10:58:23 +0800

    fix: init portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,75 @@
+import './global.css';
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import { Navbar } from './components/nav';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SandpackCSS } from './blog/[slug]/sandpack';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://leerob.io'),
+  title: {
+    default: 'Lee Robinson',
+    template: '%s | Lee Robinson',
+  },
+  description: 'Developer, writer, and creator.',
+  openGraph: {
+    title: 'Lee Robinson',
+    description: 'Developer, writer, and creator.',
+    url: 'https://leerob.io',
+    siteName: 'Lee Robinson',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'Lee Robinson',
+    card: 'summary_large_image',
+  },
+  verification: {
+    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
+    yandex: '14d2e73487fa6c71',
+  },
+};
+
+const cx = (...classes) => classes.filter(Boolean).join(' ');
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
+      <head>
+        <SandpackCSS />
+      </head>
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,79 @@
+import postgres from 'postgres';
+
+export const sql = postgres(process.env.POSTGRES_URL, {
+  ssl: 'allow',
+});
+
+const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
+  async redirects() {
+    if (!process.env.POSTGRES_URL) {
+      return [];
+    }
+
+    let redirects = await sql`
+      SELECT source, destination, permanent
+      FROM redirects;
+    `;
+
+    return redirects.map(({ source, destination, permanent }) => ({
+      source,
+      destination,
+      permanent: !!permanent,
+    }));
+  },
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self' data:;
+    frame-src 'self' *.codesandbox.io vercel.live;
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+];
+
+export default nextConfig;
