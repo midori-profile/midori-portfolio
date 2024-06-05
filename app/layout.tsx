2f7dbacfdc0f4c3d0e97de17c1dbdb7b54df9c05
ivy-tian <ceada.apple@gmail.com>
2024-06-05 17:27:52 +0900

    fix: add loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,13 @@ export default function RootLayout({
         <SandpackCSS />
       </head>
       <body className="antialiased max-w-3xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <NextNProgress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
